85e6ef7a374285db9f19e17c413084e182ac8b3f
SaiLasyaNagineni <92685570+SaiLasyaNagineni@users.noreply.github.com>
2026-01-05 21:02:22 -0600

    Delete Scalable directory

--- Scalable/TxResult.java
+++
@@ -1,7 +0,0 @@
-package com.lasya.txengine.model;
-
-public record TxResult(boolean success, boolean retryable, String message) {
-    public static TxResult ok(String msg) { return new TxResult(true, false, msg); }
-    public static TxResult retryableFail(String msg) { return new TxResult(false, true, msg); }
-    public static TxResult finalFail(String msg) { return new TxResult(false, false, msg); }
-}
--- Scalable/modelTxState.java
+++
@@ -1,9 +0,0 @@
-package com.lasya.txengine.model;
-
-public enum TxState {
-    RECEIVED,
-    PROCESSING,
-    SUCCEEDED,
-    FAILED_RETRYABLE,
-    FAILED_FINAL
-}
